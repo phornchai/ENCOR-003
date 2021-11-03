f67eb04bdeff4f56494b61fa10c68b5c6d34e948
phornchai <72152203+phornchai@users.noreply.github.com>
2021-11-03 13:22:21 +0700

    Create Netconf_Rev01.py

---
+++ Netconf_Rev01.py
@@ -0,0 +1,15 @@
+from ncclient import manager
+
+m = manager.connect(
+    host="ios-xe-mgmt.cisco.com",
+    port=10000,
+    username="developer",
+    password="C1sco12345",
+    hostkey_verify=False
+    )
+
+#test Rev03
+
+netconf_reply = m.get_config(source="running")
+print(netconf_reply)
+
